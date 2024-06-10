d6c89a53ea9fa81e496eb19fae2402762c138172
CodeWizardOve <tanvirove007@gmail.com>
2024-06-10 21:56:26 +0600

    Add files via upload

---
+++ Which is greater - Assignment 2.cpp
@@ -0,0 +1,16 @@
+//Which is Greater?
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+
+    if(a < b || a == b){
+        cout << 0;
+    }
+    else{
+        cout << 1;
+    }
+}
